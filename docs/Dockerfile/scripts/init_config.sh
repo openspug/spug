b1b951e530e8d9c447eb64b598ac502bfe242587
zyupo <yupozhang@gmail.com>
2019-05-26 14:35:57 +0800

    U - dockerfile - 更新dockerfile配置文件

--- docs/Dockerfile/scripts/init_config.sh
+++ docs/Dockerfile/scripts/init_config.sh
@@ -11,7 +11,7 @@ DOCKER_URL = 'unix:///var/run/docker.sock'
 SQLALCHEMY_ECHO = False
 
 EOF
-echo "SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://${MYSQL_USER:-spuguser}:${MYSQL_PASSWORD:-spugpwd}@localhost/${MYSQL_DATABASE:-spug}?unix_socket=/run/mysqld/mysqld.sock'" >> /spug/spug_api/config.py
+echo "SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://${MYSQL_USER:-spuguser}:${MYSQL_PASSWORD:-spugpwd}@${MYSQL_HOST:-localhost}:${MYSQL_PORT:-3306}/${MYSQL_DATABASE:-spug}?unix_socket=/run/mysqld/mysqld.sock'" >> /spug/spug_api/config.py
 echo "DOCKER_REGISTRY_SERVER = '${REGISTRY_SERVER:-hub.qbangmang.com}'" >> /spug/spug_api/config.py
 echo "DOCKER_REGISTRY_AUTH = {'username': '${REGISTRY_USER}', 'password': '${REGISTRY_PASSWORD}'}" >> /spug/spug_api/config.py
 
