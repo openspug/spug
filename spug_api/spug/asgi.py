1b617fa1462e9aebafcaa8633f92580c3fc14875
vapao <live1989@foxmail.com>
2020-01-07 21:27:11 +0800

    U api update

---
+++ spug_api/spug/asgi.py
@@ -0,0 +1,12 @@
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spug.settings')
+django.setup()
+application = get_default_application()
