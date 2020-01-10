6d158337532a4d809c9a37c89a3f0bad15707b69
vapao <live1989@foxmail.com>
2020-01-10 12:37:56 +0800

    U web update

--- spug_web/src/setupProxy.js
+++ spug_web/src/setupProxy.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
     target: 'http://127.0.0.1:8000',
     changeOrigin: true,
     ws: true,
+    headers: {'X-Real-IP': '127.0.0.1'},
     pathRewrite: {
       '^/api': ''
     }
