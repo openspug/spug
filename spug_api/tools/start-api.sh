6e26d56cf3e3f9f70271f558e2f5e95d04cd3c7d
vapao <live1989@foxmail.com>
2020-01-13 18:24:53 +0800

    U api update

--- spug_api/tools/start-api.sh
+++ spug_api/tools/start-api.sh
@@ -2,4 +2,4 @@
 # start api service
 cd $(dirname $(dirname $0))
 source ./venv/bin/activate
-exec gunicorn -b :9001 -w 2 --threads 8 --access-logfile - spug.wsgi
+exec gunicorn -b 127.0.0.1:9001 -w 2 --threads 8 --access-logfile - spug.wsgi
