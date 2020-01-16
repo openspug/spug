7ebe699d74b7dc76d2221238f63f4b0e631e3985
vapao <live1989@foxmail.com>
2020-01-16 18:46:08 +0800

    U 更新系统设置

--- spug_web/src/pages/system/setting/BasicSetting.js
+++ spug_web/src/pages/system/setting/BasicSetting.js
@@ -4,7 +4,6 @@
  * Released under the MIT License.
  */
 import React from 'react';
-import { Button, Form, Input } from 'antd';
 import styles from './index.module.css';
 
 
@@ -12,16 +11,6 @@ export default function BasicSetting(props) {
   return (
     <React.Fragment>
       <div className={styles.title}>基本设置</div>
-      <Form style={{maxWidth: 320}}>
-        <Form.Item colon={false} label="昵称">
-          <Input placeholder="请输入"/>
-        </Form.Item>
-        <Form.Item colon={false} label="邮箱">
-          <Input placeholder="请输入"/>
-        </Form.Item>
-
-        <Button type="primary">保存设置</Button>
-      </Form>
     </React.Fragment>
   )
 }
