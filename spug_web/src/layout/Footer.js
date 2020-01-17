39e64996cec28ab7a1cad24bfa08bba33a0bbb67
zypo <yupozhang@gmail.com>
2020-01-17 20:26:55 +0800

    U - 更新Footer

--- spug_web/src/layout/Footer.js
+++ spug_web/src/layout/Footer.js
@@ -12,8 +12,17 @@ export default class extends React.Component {
   render() {
     return (
       <Layout.Footer style={{padding: 0}}>
-        <div className={styles.footer}>
-          Copyright <Icon type="copyright"/> 2019 By Open Spug
+        <div className={styles.footerZone}>
+          <div className={styles.linksZone}>
+            <a className={styles.links} title="官网" href="https://www.spug.dev"  target="_blank" >官网</a>
+            <a className={styles.links} title="Github" href="https://github.com/openspug/spug"  target="_blank" >
+              <Icon type="github" />
+            </a>
+            <a title="文档" href="https://www.spug.dev/docs/about-spug/"  target="_blank" >文档</a>
+          </div>
+          <text>
+            Copyright <Icon type="copyright"/> 2019 By OpenSpug
+          </text>
         </div>
       </Layout.Footer>
     )
--- spug_web/src/pages/login/index.js
+++ spug_web/src/pages/login/index.js
@@ -100,8 +100,16 @@ class LoginIndex extends React.Component {
             loading={this.state.loading}
             onClick={this.handleSubmit}>登录</Button>
         </div>
-        <div className={styles.footer}>
-          Copyright <Icon type="copyright"/> 2019 By Open Spug
+
+        <div className={styles.footerZone}>
+          <div className={styles.linksZone}>
+            <a  className={styles.links} title="官网" href="https://www.spug.dev"  target="_blank" >官网</a>
+            <a className={styles.links}  title="Github" href="https://github.com/openspug/spug"  target="_blank" >
+              <Icon type="github" />
+            </a>
+            <a  title="文档" href="https://www.spug.dev/docs/about-spug/"  target="_blank" >文档</a>
+          </div>
+            <text >Copyright <Icon type="copyright" /> 2019 By OpenSpug</text>
         </div>
       </div>
     )
