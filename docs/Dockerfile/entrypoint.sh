8430639b6f1a38b139df4b6d64800d6f920dc93e
zypo <yupozhang@gmail.com>
2020-03-11 08:28:58 +0800

    A 添加Dockerfile

---
+++ docs/Dockerfile/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copyright: (c) OpenSpug Organization. https://github.com/openspug/spug
+# Copyright: (c) <spug.dev@gmail.com>
+# Released under the MIT License.
+
+set -e
+
+# init nginx
+if [ ! -d /run/nginx ]; then
+    mkdir -p /run/nginx
+    chown -R nginx.nginx /run/nginx
+fi
+
+# init spug
+if [ ! -f /spug/spug_api/db.sqlite3 ]; then
+    cd /spug/spug_api
+    python manage.py initdb
+    python manage.py useradd -u admin -p spug.dev -s -n 管理员
+fi
+
+nginx
+supervisord -c /etc/supervisord.conf
