bbeec1ab8caba874659839257a4565e8eb89d0ad
vapao <live1989@foxmail.com>
2020-07-18 16:03:08 +0800

    U `update` 展示版本更新日志

--- spug_api/apps/account/management/commands/update.py
+++ spug_api/apps/account/management/commands/update.py
@@ -18,6 +18,7 @@ class Command(BaseCommand):
             return self.stderr.write(self.style.ERROR('获取新版本失败，排除网络问题后请附带输出内容至官方论坛反馈'))
         if version == settings.SPUG_VERSION:
             return self.stdout.write(self.style.SUCCESS('当前已是最新版本'))
+        self.stdout.write(f'{version} 更新日志：\r\n' + res.get('body', ''))
         answer = input(f'发现新版本 {version} 是否更新[y|n]？')
         if answer.lower() != 'y':
             return
