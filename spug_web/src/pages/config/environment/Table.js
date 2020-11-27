55ba3633c851a088d765c9086bcc0e8e23560319
vapao <live1989@foxmail.com>
2020-11-27 09:55:18 +0800

    style migrate v3

--- spug_web/src/pages/config/environment/Table.js
+++ spug_web/src/pages/config/environment/Table.js
@@ -6,10 +6,10 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { Table, Modal, message } from 'antd';
-import ComForm from './Form';
+import { PlusOutlined } from '@ant-design/icons';
+import { Action, TableCard, AuthButton } from 'components';
 import { http, hasPermission } from 'libs';
 import store from './store';
-import { Action } from "components";
 
 @observer
 class ComTable extends React.Component {
@@ -37,33 +37,39 @@ class ComTable extends React.Component {
       data = data.filter(item => item['name'].toLowerCase().includes(store.f_name.toLowerCase()))
     }
     return (
-      <React.Fragment>
-        <Table
-          rowKey="id"
-          loading={store.isFetching}
-          dataSource={data}
-          pagination={{
-            showSizeChanger: true,
-            showLessItems: true,
-            hideOnSinglePage: true,
-            showTotal: total => `共 ${total} 条`,
-            pageSizeOptions: ['10', '20', '50', '100']
-          }}>
-          <Table.Column title="序号" key="series" render={(_, __, index) => index + 1}/>
-          <Table.Column title="环境名称" dataIndex="name"/>
-          <Table.Column title="标识符" dataIndex="key"/>
-          <Table.Column ellipsis title="描述信息" dataIndex="desc"/>
-          {hasPermission('config.env.edit|config.env.del') && (
-            <Table.Column title="操作" render={info => (
-              <Action>
-                <Action.Button auth="config.env.edit" onClick={() => store.showForm(info)}>编辑</Action.Button>
-                <Action.Button auth="config.env.del" onClick={() => this.handleDelete(info)}>删除</Action.Button>
-              </Action>
-            )}/>
-          )}
-        </Table>
-        {store.formVisible && <ComForm/>}
-      </React.Fragment>
+      <TableCard
+        rowKey="id"
+        title="环境列表"
+        loading={store.isFetching}
+        dataSource={data}
+        onReload={store.fetchRecords}
+        actions={[
+          <AuthButton
+            auth="config.env.add"
+            type="primary"
+            icon={<PlusOutlined/>}
+            onClick={() => store.showForm()}>新建</AuthButton>
+        ]}
+        pagination={{
+          showSizeChanger: true,
+          showLessItems: true,
+          hideOnSinglePage: true,
+          showTotal: total => `共 ${total} 条`,
+          pageSizeOptions: ['10', '20', '50', '100']
+        }}>
+        <Table.Column title="序号" key="series" render={(_, __, index) => index + 1}/>
+        <Table.Column title="环境名称" dataIndex="name"/>
+        <Table.Column title="标识符" dataIndex="key"/>
+        <Table.Column ellipsis title="描述信息" dataIndex="desc"/>
+        {hasPermission('config.env.edit|config.env.del') && (
+          <Table.Column title="操作" render={info => (
+            <Action>
+              <Action.Button auth="config.env.edit" onClick={() => store.showForm(info)}>编辑</Action.Button>
+              <Action.Button auth="config.env.del" onClick={() => this.handleDelete(info)}>删除</Action.Button>
+            </Action>
+          )}/>
+        )}
+      </TableCard>
     )
   }
 }
