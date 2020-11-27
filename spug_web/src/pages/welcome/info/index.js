ead0364f46211b60615efa6207fa3f052d6b7864
vapao <live1989@foxmail.com>
2020-11-27 16:20:05 +0800

    style migrate v3

--- spug_web/src/pages/welcome/info/index.js
+++ spug_web/src/pages/welcome/info/index.js
@@ -5,6 +5,7 @@
  */
 import React from 'react';
 import { Menu } from 'antd';
+import { Breadcrumb } from 'components';
 import Basic from './Basic';
 import Reset from './Reset';
 import styles from './index.module.css';
@@ -20,20 +21,26 @@ class Index extends React.Component {
   render() {
     const {selectedKeys} = this.state;
     return (
-      <div className={styles.container}>
-        <div className={styles.left}>
-          <Menu
-            mode="inline"
-            selectedKeys={selectedKeys}
-            style={{border: 'none'}}
-            onSelect={({selectedKeys}) => this.setState({selectedKeys})}>
-            <Menu.Item key="basic">基本设置</Menu.Item>
-            <Menu.Item key="reset">修改密码</Menu.Item>
-          </Menu>
-        </div>
-        <div className={styles.right}>
-          {selectedKeys[0] === 'basic' && <Basic />}
-          {selectedKeys[0] === 'reset' && <Reset />}
+      <div>
+        <Breadcrumb>
+          <Breadcrumb.Item>首页</Breadcrumb.Item>
+          <Breadcrumb.Item>个人中心</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <Menu
+              mode="inline"
+              selectedKeys={selectedKeys}
+              style={{border: 'none'}}
+              onSelect={({selectedKeys}) => this.setState({selectedKeys})}>
+              <Menu.Item key="basic">基本设置</Menu.Item>
+              <Menu.Item key="reset">修改密码</Menu.Item>
+            </Menu>
+          </div>
+          <div className={styles.right}>
+            {selectedKeys[0] === 'basic' && <Basic/>}
+            {selectedKeys[0] === 'reset' && <Reset/>}
+          </div>
         </div>
       </div>
     )
