7b0682a0feb5e121a97f018be1f4de85d994d952
vapao <live1989@foxmail.com>
2020-11-28 23:54:01 +0800

    style migrate v3

--- spug_web/src/pages/system/setting/BasicSetting.js
+++ spug_web/src/pages/system/setting/BasicSetting.js
@@ -5,25 +5,11 @@
  */
 import React from 'react';
 import styles from './index.module.css';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { observer } from 'mobx-react'
 
-
-@observer
-class BasicSetting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className={styles.title}>基本设置</div>
-      </React.Fragment>
-    )
-  }
+export default function () {
+  return (
+    <React.Fragment>
+      <div className={styles.title}>基本设置</div>
+    </React.Fragment>
+  )
 }
-export default Form.create()(BasicSetting)
