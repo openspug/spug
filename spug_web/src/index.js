d74c025c02eb8bbee674006a2a7adb892b3cd77c
vapao <live1989@foxmail.com>
2021-07-13 13:04:17 +0800

    format code

--- spug_web/src/index.js
+++ spug_web/src/index.js
@@ -5,27 +5,26 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router} from 'react-router-dom';
-import {ConfigProvider} from 'antd';
+import { Router } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import './index.css';
 import App from './App';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 import * as serviceWorker from './serviceWorker';
-import {history, updatePermissions} from 'libs';
+import { history, updatePermissions } from 'libs';
 
 moment.locale('zh-cn');
 updatePermissions();
 
 ReactDOM.render(
-    <Router history={history}>
-        <ConfigProvider locale={zhCN}
-                        getPopupContainer={() => document.fullscreenElement ? document.fullscreenElement : document.body}>
-            <App/>
-        </ConfigProvider>
-    </Router>,
-    document.getElementById('root')
+  <Router history={history}>
+    <ConfigProvider locale={zhCN} getPopupContainer={() => document.fullscreenElement || document.body}>
+      <App/>
+    </ConfigProvider>
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
