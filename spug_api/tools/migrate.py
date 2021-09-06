8a73879ca5b32f0000cb509699696b5618bc6632
vapao <live1989@foxmail.com>
2021-09-06 22:45:55 +0800

    fix issue

--- spug_api/tools/migrate.py
+++ spug_api/tools/migrate.py
@@ -9,6 +9,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spug.settings")
 django.setup()
 
 from django.conf import settings
+import subprocess
 import shutil
 import sys
 import os
@@ -16,8 +17,11 @@ import os
 
 if __name__ == '__main__':
     version = sys.argv[1]
-    if version < 'v3.0.1-beta.8':
+    if version <= 'v3.0.1-beta.8':
         print('执行 v3.0.1-beta.8 repos目录迁移')
         old_path = os.path.join(settings.BASE_DIR, 'repos')
         new_path = os.path.join(settings.REPOS_DIR)
         shutil.move(old_path, new_path)
+        task = subprocess.Popen(f'cd {settings.BASE_DIR} && git checkout -- repos', shell=True)
+        if task.wait() != 0:
+            print('repos目录迁移失败，请联系官方人员')
