4504ae509e40f357fba4bb7ec231a0f2e198f457
huangxinyu <huangxinyu@cmiot.chinamobile.com>
2021-09-08 16:04:03 +0800

    fix(libs/ssh): fix wrong exception message

--- spug_api/libs/ssh.py
+++ spug_api/libs/ssh.py
@@ -11,7 +11,7 @@ from io import StringIO
 class SSH:
     def __init__(self, hostname, port=SSH_PORT, username='root', pkey=None, password=None, connect_timeout=10):
         if pkey is None and password is None:
-            raise Exception('public key and password must have one is not None')
+            raise Exception('private key and password cannot both be None')
         self.client = None
         self.arguments = {
             'hostname': hostname,
