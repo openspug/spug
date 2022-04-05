b96851b2507d9ae5d52491e8027ff9d84451112d
vapao <live1989@foxmail.com>
2022-04-05 17:41:14 +0800

    U 优化样式

--- spug_web/src/pages/system/role/RoleUsers.js
+++ spug_web/src/pages/system/role/RoleUsers.js
@@ -12,11 +12,11 @@ import uStore from '../account/store';
 export default observer(function (props) {
   const users = uStore.records.filter(x => x.role_ids.includes(props.id))
   return (
-    <Table rowKey="id" dataSource={users} pagination={false} scroll={{y: 500,x: 500}}>
-      <Table.Column title="姓名" dataIndex="nickname"/>
-      <Table.Column title="状态" dataIndex="is_active"
+    <Table rowKey="id" dataSource={users} pagination={false} scroll={{y: 500}}>
+      <Table.Column width={120} title="姓名" dataIndex="nickname"/>
+      <Table.Column width={90} title="状态" dataIndex="is_active"
                     render={v => v ? <Badge status="success" text="正常"/> : <Badge status="default" text="禁用"/>}/>
-      <Table.Column title="最近登录" dataIndex="last_login"/>
+      <Table.Column width={180} title="最近登录" dataIndex="last_login"/>
     </Table>
   )
 })
