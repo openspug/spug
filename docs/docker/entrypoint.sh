58f60d126e010724af88222f6d2bf89485805481
vapao <live1989@foxmail.com>
2022-08-10 16:38:51 +0800

    Update docs/docker

--- docs/docker/entrypoint.sh
+++ docs/docker/entrypoint.sh
@@ -6,10 +6,16 @@ if [ -e /root/.bashrc ]; then
     source /root/.bashrc
 fi
 
-if [ ! -d /data/spug ]; then
-    tar xf /tmp/spug.tar.gz -C /data/
+if [ ! -d /data/spug/spug_api ]; then
+    git clone -b $SPUG_DOCKER_VERSION https://gitee.com/openspug/spug.git /data/spug
+    curl -o web.tar.gz https://cdn.spug.cc/spug/web_${SPUG_DOCKER_VERSION}.tar.gz
+    tar xf web.tar.gz -C /data/spug/spug_web/
+    rm -f web.tar.gz
     SECRET_KEY=$(< /dev/urandom tr -dc '!@#%^.a-zA-Z0-9' | head -c50)
-    cat > /data/spug/spug_api/spug//overrides.py << EOF
+    cat > /data/spug/spug_api/spug/overrides.py << EOF
+import os
+
+
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1']
 SECRET_KEY = '${SECRET_KEY}'
@@ -18,36 +24,18 @@ DATABASES = {
     'default': {
         'ATOMIC_REQUESTS': True,
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'spug',
-        'USER': 'spug',
-        'PASSWORD': 'spug.dev',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT'),
         'OPTIONS': {
-            'unix_socket': '/var/lib/mysql/mysql.sock',
             'charset': 'utf8mb4',
             'sql_mode': 'STRICT_TRANS_TABLES',
         }
     }
 }
 EOF
-fi
-
-if [ ! -d /data/mysql ]; then
-    mkdir -p /data/mysql
-    mysql_install_db
-    chown -R mysql.mysql /data/mysql
-
-    tfile=`mktemp`
-    cat >> $tfile << EOF
-use mysql;
-flush privileges;
-create database spug character set utf8mb4 collate utf8mb4_unicode_ci;
-grant all on spug.* to spug@'localhost' identified by 'spug.dev';
-flush privileges;
-EOF
-
-    /usr/libexec/mysqld --user=mysql --bootstrap < $tfile
-    rm -f $tfile
 fi
 
 exec supervisord -c /etc/supervisord.conf
