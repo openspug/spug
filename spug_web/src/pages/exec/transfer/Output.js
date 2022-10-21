375bbc6127816f20608ac5540178e8761818bee1
vapao <live1989@foxmail.com>
2022-10-21 16:47:41 +0800

    U 优化文件分发在文件过多时展示效果

--- spug_web/src/pages/exec/transfer/Output.js
+++ spug_web/src/pages/exec/transfer/Output.js
@@ -32,7 +32,8 @@ function OutView(props) {
     store.tag = ''
     gCurrent = current
     term.setOption('disableStdin', true)
-    term.setOption('fontSize', gStore.terminal.fontSize)
+    term.setOption('fontSize', 14)
+    term.setOption('lineHeight', 1.2)
     term.setOption('fontFamily', gStore.terminal.fontFamily)
     term.setOption('theme', {background: '#2b2b2b', foreground: '#A9B7C6', cursor: '#2b2b2b'})
     term.attachCustomKeyEventHandler((arg) => {
