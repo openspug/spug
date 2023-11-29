4666795a28d36671a527bb87963f463835b3d2fb
vapao <live1989@foxmail.com>
2023-11-29 16:15:27 +0800

    # Release v3.3.2

--- spug_api/spug/settings.py
+++ spug_api/spug/settings.py
@@ -133,7 +133,7 @@ AUTHENTICATION_EXCLUDES = (
     re.compile('/apis/.*'),
 )
 
-SPUG_VERSION = 'v3.3.1'
+SPUG_VERSION = 'v3.3.2'
 
 # override default config
 try:
--- spug_web/src/libs/index.js
+++ spug_web/src/libs/index.js
@@ -10,4 +10,4 @@ export * from './functools';
 export * from './router';
 export const http = _http;
 export const history = _history;
-export const VERSION = 'v3.3.1';
+export const VERSION = 'v3.3.2';
